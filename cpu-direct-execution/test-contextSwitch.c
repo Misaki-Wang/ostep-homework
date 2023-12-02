7ca3438e8e268d0ea92e76e88c43ffe574ee208b
misaki <misaki-wang@qq.com>
2023-12-02 15:42:28 +0800

    cpu-direct-execution

---
+++ cpu-direct-execution/test-contextSwitch.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/time.h>
+#include <sys/types.h>
+
+#define ITERATIONS 10000
+
+int main() {
+    int pipe1[2], pipe2[2];
+    char buf;
+    struct timeval start, end;
+    pid_t pid;
+    long seconds, useconds;
+    double total_time, average_time;
+
+    // 创建两个管道
+    if (pipe(pipe1) == -1 || pipe(pipe2) == -1) {
+        perror("pipe");
+        return 1;
+    }
+
+    // 创建子进程
+    pid = fork();
+    if (pid < 0) {
+        perror("fork");
+        return 1;
+    }
+
+    if (pid == 0) {  // 子进程
+        for (int i = 0; i < ITERATIONS; i++) {
+            read(pipe1[0], &buf, 1);      // 从 pipe1 读取
+            write(pipe2[1], &buf, 1);     // 写入 pipe2
+        }
+    } 
+    else {         // 父进程
+        gettimeofday(&start, NULL);     // 记录开始时间
+
+        for (int i = 0; i < ITERATIONS; i++) {
+            write(pipe1[1], &buf, 1);    // 写入 pipe1
+            read(pipe2[0], &buf, 1);     // 从 pipe2 读取
+        }
+
+        gettimeofday(&end, NULL);       // 记录结束时间
+
+        // 计算总耗时
+        seconds  = end.tv_sec  - start.tv_sec;
+        useconds = end.tv_usec - start.tv_usec;
+        total_time = ((seconds) * 1000 + useconds / 1000.0) + 0.5;
+
+        // 计算平均耗时
+        average_time = total_time / ITERATIONS;
+
+        printf("Average context switch time: %f milliseconds\n", average_time);
+    }
+
+    return 0;
+}
---
+++ cpu-direct-execution/test-syscall.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <sys/time.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+int main() {
+    int fd = open("/dev/null", O_RDONLY); // 打开 /dev/null 用于读取
+    if (fd < 0) {
+        perror("Failed to open /dev/null");
+        return 1;
+    }
+
+    struct timeval start, end;
+    long seconds, useconds;
+    double total_time, average_time;
+    int iterations = 10000000000; // 设置迭代次数
+
+    // 记录开始时间
+    gettimeofday(&start, NULL);
+
+    for (int i = 0; i < iterations; i++) {
+        read(fd, NULL, 0); // 执行 0 字节的读取
+    }
+
+    // 记录结束时间
+    gettimeofday(&end, NULL);
+
+    // 计算总耗时
+    seconds  = end.tv_sec  - start.tv_sec;
+    useconds = end.tv_usec - start.tv_usec;
+    total_time = ((seconds) * 1000 + useconds/1000.0) + 0.5; // 加上 0.5 是为了在转换到毫秒时进行四舍五入 
+
+    // 计算平均耗时
+    average_time = total_time / iterations;
+
+    printf("Average time per system call: %f milliseconds\n", average_time);
+
+    close(fd);
+    return 0;
+}
